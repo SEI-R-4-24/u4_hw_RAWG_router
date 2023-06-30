5cb587f58f33b08993f4b2c31c4557671059c5be
Cherch <b.cherchiglia@gmail.com>
2023-06-30 13:51:58 -0400

    part 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react'
 import './styles/App.css'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import GameDetails from './pages/GameDetails'
+import ViewGames from './pages/ViewGames'
 
 const App = () => {
 
   return (
     <div>
-      <p>sup</p>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/gamedetails" element={<GameDetails />} />
+          <Route path="/viewgames" element={<ViewGames />} />
+        </Routes>
+      </main>
     </div>
   )
 }
