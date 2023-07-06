af8f7126ac7f0ba8bcdeda418b7ae7d815589bb3
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-07-06 15:28:35 -0400

    Home Page Setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,11 @@
 import { useState } from 'react'
+import axios from 'axios'
+import { Search } from 'react-router-dom'
+import GameCard from '../components/GameCard'
+import GenreCard from '../components/GenreCard'
+
+
+const API_KEY = import.meta.env.VITE_RAWG_KEY
 
 const Home = () => {
   const [genres, setGenres] = useState([])
@@ -6,30 +13,60 @@ const Home = () => {
   const [searched, toggleSearched] = useState(false)
   const [searchQuery, setSearchQuery] = useState('')
 
+  useEffect(() => {
+    getGenres()
+  }, []
+  )
+
   const getGenres = async () => {
-
+    let genreResults = await axios.get('https://api.rawg.io/api/genres?key=${API_KEY}')
+    console.log(genreResults.data.results)
+    setGenres(genreResults.data.results)
   }
 
-  const getSearchResults = async (e) => {
-    e.preventDefault()
+  const getSearchResults = async () => {
+    let genreResults = await axios.get('https://api.rawg.io/api/genres?key=${API_KEY}&search=${searchQuery}')
+    console.log(results)
+    setSearchResults(results.data.results)
   }
-
-  const handleChange = (event) => {
-
+  
+  const handleChange = (evt) => {
+    setSearchQuery(evt.target.value)
   }
 
   return (
     <div>
       <div className="search">
+        <Search 
+        onSubmit={getSearchResults}
+        onChange={handleChange}
+        value={searchQuery}
+        />
         <h2>Search Results</h2>
         <section className="search-results container-grid">
-
+          <div>
+            {
+              searchResults.map((searchResults) => {
+                return (<GameCard 
+                  game={searchResult}
+                  />)
+              })
+            }
+          </div>
         </section>
       </div>
       <div className="genres">
         <h2>Genres</h2>
         <section className="container-grid">
-
+        <div>
+          {
+            genres.map((genre) => {
+              return (<GenreCard
+                genre={genre}
+                />)
+            })
+          }
+        </div>
         </section>
       </div>
     </div>
