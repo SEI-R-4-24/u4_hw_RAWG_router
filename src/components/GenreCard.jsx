47ed668224aeca1ea87dcec4939ddb6cc1e610df
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-07-06 15:06:38 -0400

    Genrecard Setup

--- src/components/GenreCard.jsx
+++ src/components/GenreCard.jsx
@@ -1,13 +1,19 @@
-const GenreCard = () => {
+import { Link } from "react-router-dom"
+
+const GenreCard = ({ genre }) => {
 
   return (
     <div className="card">
       <div className="img-wrapper">
-
+        <img className="gmae-image" src={genre.image_background} alt={genre.name} />
       </div>
       <div className="info-wrapper flex-col">
-        
+        <h3>{genre.name}</h3>
+        <p>Games: {genre.games_count}</p>
       </div>
+      <Link to="/viewgames">
+        <button className="backButton">Back</button>
+      </Link>
     </div>
   )
 }
