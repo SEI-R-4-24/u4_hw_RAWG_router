3d1f40bbc586b2cea1ed4bb4757397c7e258c5d8
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-07-06 15:01:37 -0400

    Gamecard Setup

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,38 +1,21 @@
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-const GameCard = ({games}) => {
-  let {id} = useParams()
-
-  const [gameId, setGameId] = useState('')
-  const [game, setGame] = useState(null)
-
-  useEffect(() => {
-    let selectedGame = games.find(
-      (game) => game.id === parseInt(id)
-    )
-    setGame(selectedGame)
-    let selectedGameId = games.find(
-      (game) => game.id === parseInt(id)
-    )
-    setGameId(selectedGameId)
-  })
-  
-  return (
-    <div className="card game-card" onClick={games.onClick}>
+const GameCard = ({game}) => {
+  console.log(game)
+  return game ? (
+    <div className="card game-card" onClick={game.id.onClick}>
       <div className="img-wrapper">
-        <img src={games.image} alt={game.name} />
+        <img src={game.background_image} alt={game.name} />
       </div>
       <div className="info-wrapper flex-col">
-        <h3>{games.name}</h3>
+        <h3>{game.name}</h3>
       </div>
       <Link to="/viewgames">
         <button className='backButton'> Back </button>
       </Link>
     </div>
-  )
+  ) : null
 }
 
 export default GameCard
